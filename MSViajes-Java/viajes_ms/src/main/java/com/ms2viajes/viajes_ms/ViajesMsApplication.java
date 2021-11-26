17a8e190d78a6d0f1f82bae88a5e2b216b20a221
Sebastian <83480074+ingsebastiancastilla@users.noreply.github.com>
2021-11-26 16:00:27 -0500

    Set up del proyecto Microservicio 2

---
+++ MSViajes-Java/viajes_ms/src/main/java/com/ms2viajes/viajes_ms/ViajesMsApplication.java
@@ -0,0 +1,13 @@
+package com.ms2viajes.viajes_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajesMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajesMsApplication.class, args);
+	}
+
+}
---
+++ MSViajes-Java/viajes_ms/src/test/java/com/ms2viajes/viajes_ms/ViajesMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms2viajes.viajes_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajesMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
